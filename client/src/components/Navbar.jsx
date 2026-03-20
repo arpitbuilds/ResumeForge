6960ad0d6fc02e7b4f48624d75bb1e18dbc7acc5
Arpit <arpitbuilds@gmail.com>
2026-03-20 23:15:36 +0530

    Update internal navbar logo to ResumeForge

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,11 +20,9 @@ const Navbar = () => {
       <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all">
         {/* Logo Link: Always redirects to the home page (/). */}
         <Link to="/">
-          <img
-            src="/logo.svg"
-            alt="Resume Builder Logo"
-            className="w-11 w-auto"
-          />
+          <span className="font-bold text-xl">
+            Resume<span className="text-primary">Forge</span>
+          </span>
         </Link>
 
         {/* User Info and Action Buttons */}
